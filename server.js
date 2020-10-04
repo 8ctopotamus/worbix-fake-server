4c5fb994f9756412a08f2eefb822dc20fab8e93d
Josh <zylo.codes@gmail.com>
2020-10-03 19:49:43 -0500

    first

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/createArticle', (req, res) => {
+  console.log(req.body)
+  res.status(200).send()
+})
+
+app.listen(port, () => console.info(`Worbix Fake Server listening @ http://localhost:${port}`))
