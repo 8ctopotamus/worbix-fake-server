14fff76e4f4f8115acb8d5398a34656fdd843e7b
Josh <zylo.codes@gmail.com>
2020-10-04 08:52:23 -0500

    C:/Program Files/Git/managePlan, refactors callback

--- server.js
+++ server.js
@@ -7,18 +7,14 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/createArticle', (req, res) => {
-  console.log(req.body)
+const callback = (req, res) => {
+  console.log(`[${req.url}]`, req.body)
   res.status(200).send()
-})
+}
 
-app.post('/subscribe', (req, res) => {
-  console.log(req.body)
-  res.status(200).send()
-})
+app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/createArticle', callback)
+app.post('/subscribe', callback)
+app.post('/managePlan', callback)
 
 app.listen(port, () => console.info(`Worbix Fake Server listening @ http://localhost:${port}`))
